b9e1e551ea8f66abeeb76e66c144a172acbbbd2e
Luiz Wanderson <38147355+ProHatp@users.noreply.github.com>
2026-05-15 04:32:06 -0300

    BKP

--- get_images.js
+++ get_images.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import https from "https";
 import puppeteer from "puppeteer";
 
-const WIKI_URL = "https://blox-fruits.fandom.com/wiki/Fiend";
+const WIKI_URL = "https://blox-fruits.fandom.com/wiki/Easter_Egg_Hunt";
 const DIR = "./assets/imgs";
 
 function limparNome(url) {
